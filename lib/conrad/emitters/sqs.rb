1cdee50a9d43b25102dfc0877a72955783b1f9de
Joseph Sessions <joseph.sessions@outreach.io>
2019-01-03 14:30:34 -0800

    Appeases the linter

--- lib/conrad/emitters/sqs.rb
+++ lib/conrad/emitters/sqs.rb
@@ -4,9 +4,10 @@ module Conrad
     class Sqs
       def call(event)
         sqs = Aws::SQS::Client.new(
-            region: ENV['SQS_REGION'],
-            access_key_id: ENV['SQS_ACCESS_KEY_ID'],
-            secret_access_key: ENV['SQS_SECRET_ACCESS_KEY'])
+          region: ENV['SQS_REGION'],
+          access_key_id: ENV['SQS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['SQS_SECRET_ACCESS_KEY']
+        )
         sqs.send_message(queue_url: ENV['SQS_QUEUE_URL'], message_body: event)
       end
     end
